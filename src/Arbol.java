db9367f9abb5d9e051d1deede5b06b3234df9c49
Filip Piworowicz <Filip Piworowicz@HZ114829.H012108.NET>
2023-01-17 14:26:12 +0100

    CLASE ARBOL CREADA, AHORA HACIENDO EL MENU LUEGO AÑADIR EL SQL

--- src/Arbol.java
+++ src/Arbol.java
@@ -1,6 +1,72 @@
 
 public class Arbol {
 
-	
-	
+	private int ID;
+	private String nombre_comun;
+	private String nombre_cientifico;
+	private String habitat;
+	private int altura;
+	private String origen;
+
+	public Arbol() {
+
+	}
+
+	public Arbol(int ID, String nombre_comun, String nombre_cientifico, String habitat, int altura, String origen) {
+		this.ID = ID;
+		this.nombre_comun = nombre_comun;
+		this.nombre_cientifico = nombre_cientifico;
+		this.habitat = habitat;
+		this.altura = altura;
+		this.origen = origen;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getNombre_comun() {
+		return nombre_comun;
+	}
+
+	public void setNombre_comun(String nombre_comun) {
+		this.nombre_comun = nombre_comun;
+	}
+
+	public String getNombre_cientifico() {
+		return nombre_cientifico;
+	}
+
+	public void setNombre_cientifico(String nombre_cientifico) {
+		this.nombre_cientifico = nombre_cientifico;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
 }
--- src/GestorArboles.java
+++ src/GestorArboles.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.sql.*;
 
 public class GestorArboles {
 
+	private static final int INSERTAR_ARBOL = 1;
+	private static final int ELIMINAR_ARBOL = 2;
+	private static final int MODIFICAR_ARBOL = 3;
+	private static final int VISUALIZAR_ARBOL = 4;
+	private static final int SALIR = 5;
+
 	public void run() {
 		
+		ArrayList<Arbol> arboles = new ArrayList<Arbol>();
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int opc = Integer.parseInt(sc.nextLine());
+		
+		do {
+			
+		}while(opc != SALIR);
 		
 	}
 
