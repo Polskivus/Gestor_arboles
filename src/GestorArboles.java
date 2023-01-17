837f50100be1618744d34ebc49500fe4fb10e6b2
Filip Piworowicz <Filip Piworowicz@HZ114829.H012108.NET>
2023-01-17 14:08:15 +0100

    First commit, hemos creado el ejecutable ahora crear la clase arbol y luego hacer el menu en el run(GestorArboles)

---
+++ src/GestorArboles.java
@@ -0,0 +1,10 @@
+
+public class GestorArboles {
+
+	public void run() {
+		
+		
+		
+	}
+
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+
+public class main {
+	public static void Main(String[] args) {
+		GestorArboles gf = new GestorArboles();
+		
+		gf.run();
+	}
+}
